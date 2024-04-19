e16dbc2fe90deb6e4a9de7bb1c48234f30335541
sidaliam <sidaliamo18@gmail.com>
2024-04-19 19:42:26 +0100

    commit 2

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import authroute from "./routes/auth.js"
-import usersroute from "./routes/users.js"
-import orderroute from "./routes/Order.js"
-import hotelsroute from "./routes/hotels.js"
-import roomsroute from "./routes/rooms.js"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import authroute from "./routes/auth.js";
+import usersroute from "./routes/users.js";
+import orderroute from "./routes/Order.js";
+import hotelsroute from "./routes/hotels.js";
+import roomsroute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
-    console.log("connecté a mongodb!")
+    console.log("connecté a mongodb!");
   } catch (error) {
     throw error;
   }
 };
-app.use(cors());
-app.use(cookieParser())
-app.use(express.json())
-app.use("/api/auth",authroute)
-app.use("/api/hotels",hotelsroute)
-app.use("/api/rooms",roomsroute)
-app.use("/api/users",usersroute)
-app.use("/api/orders",orderroute)
+// Configuration CORS pour autoriser uniquement une origine spécifique
+const corsOptions = {
+  origin: "http://localhost:8000", // Remplacez par le domaine de votre frontend déployé
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
 
-
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth", authroute);
+app.use("/api/hotels", hotelsroute);
+app.use("/api/rooms", roomsroute);
+app.use("/api/users", usersroute);
+app.use("/api/orders", orderroute);
 
 app.listen(process.env.PORT || 5000, () => {
   connect();
   console.log("connecté au serveur");
 });
-
-
